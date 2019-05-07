458571de765c7cf8b22fb42e74d8f65d66671e82
wop129@tom.com <wop129@tom.com>
2019-05-07 18:08:41 +0800

    modify

--- a-service/src/main/java/com/guppy/a/controller/ServiceAController.java
+++ a-service/src/main/java/com/guppy/a/controller/ServiceAController.java
@@ -26,8 +26,14 @@ public class ServiceAController {
     @GetMapping(value = "/")
     public String printServiceA(){
         ServiceInstance serviceInstance = discoveryClient.getLocalServiceInstance();
-        return serviceInstance.getServiceId() + " (" + serviceInstance.getHost() + ":" + serviceInstance.getPort()
-                + ") ===>name:" + name + "<br/>" + serviceBClient.printServiceB();
+        return serviceInstance.getServiceId()
+                + " (" + serviceInstance.getHost()
+                + ":"
+                + serviceInstance.getPort()
+                + ") ===>name:"
+                + name
+                + "<br/>"
+                + serviceBClient.printServiceB();
     }
 
     @GetMapping(path = "/current")
--- b-service/src/main/java/com/guppy/b/controller/ServiceBController.java
+++ b-service/src/main/java/com/guppy/b/controller/ServiceBController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
@@ -16,6 +17,7 @@ public class ServiceBController {
     @Value(value = "${msg:unknown}")
     private String msg;
 
+    @GetMapping(value = "/")
     public String printServiceB(){
         ServiceInstance serviceInstance = discoveryClient.getLocalServiceInstance();
         return serviceInstance.getServiceId()
